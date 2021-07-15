b7365527c28e62be236dcbaaecb817febd7628fb
gobhetio07 <gael.valla16@gmail.com>
2021-07-15 21:42:04 +0200

    1st commit
    tout le projet

---
+++ Assets/script/movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement: MonoBehaviour
+{
+    public float speed = 5f;
+    Rigidbody2D rb;
+    Vector2 dir;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        dir.x = Input.GetAxisRaw("Horizontal");
+        dir.y = Input.GetAxisRaw("Vertical");
+        rb.MovePosition(rb.position + dir * speed * Time.fixedDeltaTime);
+    }
+
+}
